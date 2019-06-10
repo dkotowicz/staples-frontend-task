c21b879a3d7d146836217d14b4dbfd5524b4c5f8
Daria Kotowicz <dasha.kotovich@gmail.com>
2019-06-10 22:23:31 +0200

    add tailwind style Alert

--- client/src/components/Alert.vue
+++ client/src/components/Alert.vue
@@ -1,15 +1,15 @@
 <template>
-<div>
-  <p class="">{{alert}}</p>
+<div class="fixed overflow-auto w-1/4 sm:w-full md:w-full lg:w-1/4 xl:w-1/4 inset-0 z-20 items-central mx-auto h-12 sm:h-24 md:h-24 lg:h-12 xl:h-12 bg-gray-600 rounded-b">
+  <p class="text-white text-center items-central pt-2 text-xl sm:text-5xl md:text-5xl lg:text-xl xl:text-xl">{{alert}}</p>
 </div>
 </template>
 
 <script>
 export default {
   computed: {
-        alert() {
-            return this.$store.getters.alertText
-        }
-    },
+    alert() {
+      return this.$store.getters.alertText
+    }
+  },
 }
 </script>
