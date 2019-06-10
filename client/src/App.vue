f3b4605419b0450e26b1014dac3da3325fd25b81
Daria Kotowicz <dasha.kotovich@gmail.com>
2019-06-10 22:36:27 +0200

    add Alert to components in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,7 +14,8 @@ export default {
     name: 'app',
     components: {
         ProductList,
-        ProductDetails
+        ProductDetails,
+        Alert
     },
     computed: {
         showProductDetails() {
